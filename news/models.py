fd5997ed7f31c1c307ff4701115089f210285dd7
nurlan5t <nur23kg@mail.ru>
2021-12-22 06:00:30 +0600

    created: news (model, migrations)

--- news/models.py
+++ news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    link = models.URLField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    upvotes = models.PositiveIntegerField()
+    author = models.ForeignKey('auth.user', related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
